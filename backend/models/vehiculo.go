952e1930923bb02d517820a384a97a451106953a
ErnestoRdS <egeregogbu@gmail.com>
2023-05-07 16:07:04 -0600

    Se añadieron los modelos faltantes para la estructura de datos en el backend

---
+++ backend/models/vehiculo.go
@@ -0,0 +1,15 @@
+package models
+
+import "gorm.io/gorm"
+
+type Vehiculo struct {
+	gorm.Model
+	Fabricante string `json:"Fabricante"`
+	Marca string `json:"Marca"`
+	Modelo uint `json:"Modelo"`
+	Placas string `json:"Placas"`
+	Color string `json:"Color"`
+	VigenciaTec string `json:"VigenciaTec"`
+	Seguro bool `json:"Seguro"`
+	Estado string `json:"Estado"`
+}
---
+++ backend/models/viaje.go
@@ -0,0 +1,13 @@
+package models
+
+import "gorm.io/gorm"
+
+type Viaje struct {
+	gorm.Model
+	ConductorID uint `json:"-"`
+	VehiculoID uint `json:"-"`
+	Fecha string `json:"Fecha"`
+	Origen string `json:"Origen"`
+	Destino string `json:"Destino"`
+	Monto float64 `json:"Monto"`
+}
