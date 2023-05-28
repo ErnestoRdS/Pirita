698b164c67224e1f5bbdd4e09a10b81a697d155b
VentGrey <omarpurataf@gmail.com>
2023-05-27 19:20:47 -0600

    Formateados los archivos de TS para que no fallen las pipelines

--- frontend/src/lib/types/conductor.d.ts
+++ frontend/src/lib/types/conductor.d.ts
@@ -1,15 +1,15 @@
 interface Conductor {
-  id: number;
-  nombre: string;
-  apellidos: string;
-  curp: string;
-  clave_ine: string;
-  salario: number;
-  estado: string;
-  pagos?: Pago[];
-  usuario: string;
-  correo: string;
-  password: string;
+	id: number;
+	nombre: string;
+	apellidos: string;
+	curp: string;
+	clave_ine: string;
+	salario: number;
+	estado: string;
+	pagos?: Pago[];
+	usuario: string;
+	correo: string;
+	password: string;
 }
 
 export default Conductor;
