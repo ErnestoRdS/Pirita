ea26074fadd9c1044d19b160de623c77e1c7b677
VentGrey <omarpurataf@gmail.com>
2023-05-27 23:36:24 -0600

    Añadidas rutas de frontend

---
+++ frontend/src/lib/helpers/share.ts
@@ -0,0 +1,19 @@
+/**
+ * Opens a "Share" dialog on specific pages on sharing-capable
+ * browsers, mostly mobile browsers.
+ *
+ * @export
+ * @param {string} shareText
+ * @return {*}  {Promise<void>}
+ */
+export async function share(shareText: string): Promise<void> {
+	if (typeof navigator.canShare !== 'undefined') {
+		navigator.share({
+			title: 'UpVent Technologies',
+			text: shareText,
+			url: 'https://upvent.codes/'
+		});
+	} else {
+		return;
+	}
+}
