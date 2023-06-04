1ac851969ac83481d69b5198d3432de0d0c4a9a1
VentGrey <omarpurataf@gmail.com>
2023-06-04 16:41:54 -0600

    Corregir el inicio de sesión de los conductores
    - Se añadió una ruta de inicio de sesión para los conductores

--- backend/routes/login.go
+++ backend/routes/login.go
@@ -32,48 +32,87 @@ func LoginRouter(app *fiber.App, db *gorm.DB) {
 		}
 
 		var admin models.Administrador
-		var conductor models.Conductor
 
 		// Buscar al usuario en la base de datos.
 		if err := db.Where("usuario = ?", input.Usuario).First(&admin).Error; err != nil {
-			if err := db.Where("usuario = ?", input.Usuario).First(&conductor).Error; err != nil {
-				return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-					"message": "Usuario no encontrado.",
-				})
-			} else {
-				// Comprobar la contraseña del conductor.
-				if err := bcrypt.CompareHashAndPassword([]byte(conductor.Password), []byte(input.Password)); err != nil {
-					return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-						"message": "Contraseña incorrecta.",
-					})
-				}
-			}
-		} else {
-			// Comprobar la contraseña del administrador.
-			if err := bcrypt.CompareHashAndPassword([]byte(admin.Password), []byte(input.Password)); err != nil {
-				return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-					"message": "Contraseña incorrecta.",
-				})
-			}
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"message": "Usuario no encontrado.",
+			})
+		}
+
+		// Comprobar la contraseña del administrador.
+		if err := bcrypt.CompareHashAndPassword([]byte(admin.Password), []byte(input.Password)); err != nil {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+				"message": "Contraseña incorrecta.",
+			})
 		}
 
 		// Crear el token.
 		token := jwt.New(jwt.SigningMethodHS256)
 
 		// Añadir los claimms dependiendo del tipo de usuario.
-		if admin.ID != 0 {
-			claims := token.Claims.(jwt.MapClaims)
-			claims["id"] = admin.ID
-			claims["user"] = admin.Usuario
-			claims["type"] = "admin"
-			claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
-		} else {
-			claims := token.Claims.(jwt.MapClaims)
-			claims["id"] = conductor.ID
-			claims["user"] = conductor.Usuario
-			claims["type"] = "conductor"
-			claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+		claims := token.Claims.(jwt.MapClaims)
+		claims["id"] = admin.ID
+		claims["user"] = admin.Usuario
+		claims["type"] = "admin"
+		claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+
+		secret := []byte(os.Getenv("JWT_SECRET"))
+
+		// Firmar el token.
+		tokenString, err := token.SignedString(secret)
+
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"message": "Error al firmar el token.",
+			})
 		}
+
+		// Devolver el token.
+		return c.JSON(fiber.Map{
+			"token": tokenString,
+		})
+	})
+
+	app.Post("/loginconductor", func(c *fiber.Ctx) error {
+		type LoginInput struct {
+			Usuario  string `json:"usuario"`
+			Password string `json:"password"`
+		}
+
+		input := new(LoginInput)
+
+		if err := c.BodyParser(input); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"message": "Error al decodificar el cuerpo de la petición",
+			})
+		}
+
+		var conductor models.Conductor
+
+		// Buscar al usuario en la base de datos.
+		if err := db.Where("usuario = ?", input.Usuario).First(&conductor).Error; err != nil {
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"message": "Usuario no encontrado.",
+			})
+		}
+
+		// Comprobar la contraseña del conductor.
+		if err := bcrypt.CompareHashAndPassword([]byte(conductor.Password), []byte(input.Password)); err != nil {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+				"message": "Contraseña incorrecta.",
+			})
+		}
+
+		// Crear el token.
+		token := jwt.New(jwt.SigningMethodHS256)
+
+		// Añadir los claims para el conductor.
+		claims := token.Claims.(jwt.MapClaims)
+		claims["id"] = conductor.ID
+		claims["user"] = conductor.Usuario
+		claims["type"] = "conductor"
+		claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
 
 		secret := []byte(os.Getenv("JWT_SECRET"))
 
--- scripts/test_login.sh
+++ scripts/test_login.sh
@@ -8,24 +8,43 @@ http_request() {
   local method=$1
   local url=$2
   local body=$3
-  local response=$(curl -X "$method" -s -H "Content-Type: application/json" -d "$body" "$url")
+  local response
+
+  response=$(curl -X "$method" -s -H "Content-Type: application/json" -d "$body" "$url")
   echo "$response"
 }
 
-# Función para autenticar como un usuario
-authenticate_user() {
+# Función para autenticar como un administrador
+authenticate_admin() {
   local username=$1
   local password=$2
-  local response=$(http_request "POST" "$API_BASE_URL/login" "{\"usuario\": \"$username\", \"password\": \"$password\"}")
+  local response
+  response=$(http_request "POST" "$API_BASE_URL/login" "{\"usuario\": \"$username\", \"password\": \"$password\"}")
 
   if [ -z "$response" ]; then
     echo "Error: No se pudo realizar la solicitud HTTP"
     exit 1
   else
-    echo "Se inició sesión correctamente como $username con la contraseña $password y el token $response"
+    echo "Se inició sesión correctamente como admin $username con la contraseña $password y el token $response"
+  fi
+}
+
+# Función para autenticar como un conductor
+authenticate_conductor() {
+  local username=$1
+  local password=$2
+  local response
+  response=$(http_request "POST" "$API_BASE_URL/loginconductor" "{\"usuario\": \"$username\", \"password\": \"$password\"}")
+
+  if [ -z "$response" ]; then
+    echo "Error: No se pudo realizar la solicitud HTTP"
+    exit 1
+  else
+    echo "Se inició sesión correctamente como conductor $username con la contraseña $password y el token $response"
   fi
 }
 
 # Ejemplo de uso
-authenticate_user "admin" "PiritaAdmin"
-authenticate_user "conductor" "conductordemo"
+authenticate_admin "admin" "PiritaAdmin"
+echo ""
+authenticate_conductor "conductor" "conductordemo"
